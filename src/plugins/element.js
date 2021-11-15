c8265dfd05bfb5bb010a7b7ad33974fb44a32781
lunarloong <lzhlovests4444@gmail.com>
2021-11-15 16:56:26 +0800

    登录页面完成（无token授权）

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,14 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import {Button,Form,FormItem,Input,Container,Header,Aside,Main,Drawer,Avatar,Divider} from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Drawer)
+Vue.use(Avatar)
+Vue.use(Divider)
